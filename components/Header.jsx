62a217ded1088496cac0d0faa4e620ca0f2f3d55
zheer barzan <zheerbarzan85@outlook.com>
2024-10-15 17:47:35 +0300

    wont render

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,18 @@
 import Link from "next/link";
+import { Button } from "./ui/button";
+import { Span } from "next/dist/trace";
 
 const Header = () => {
-    return <header>Header</header>
+    return (
+    <header className="py-8 xl:py-12 text-white ">
+        <div className="container mx-auto">
+            <Link href="/">
+            <h1 className="text-4xl font-semibold">Zheer Barzan <span>.</span> </h1>
+            </Link>
+            header
+        </div>
+    </header>
+    );
 }
 
 export default Header; 
